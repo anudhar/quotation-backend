f7fe0f1218bc38d2e69cc1bede903c9b3bda8d6a
anudhar <msanudhar@gmail.com>
2021-05-13 13:00:00 +0530

    first commit

---
+++ quotation-backend/src/main/java/com/uc/quotationbackend/QuotationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uc.quotationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationBackendApplication.class, args);
+	}
+
+}
